4cd6f7a9dde2c7a3da1702dac8d5bc5ce6e9152c
VinayakTekade <vinayak.tekade@gmail.com>
2019-09-11 05:48:31 +0530

    added services

--- js/main.js
+++ js/main.js
@@ -16,3 +16,14 @@ if($('.navbar').length > 0){
         checkScroll();
     });
 }
+
+$("#owl-slider").owlCarousel({
+    navigation: false,
+    pagination: true,
+    itemsCustom : [
+        [0, 1],
+        [700, 2],
+        [960, 3]
+     ],
+    navigationText: false
+});
